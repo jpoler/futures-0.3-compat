c0e34c500b4b401ea4412a408f293a3ac13716d7
Jon Poler <jonathan.poler@gmail.com>
2018-08-04 23:31:52 -0700

    A few tweaks just for clarity

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,8 @@
-#![feature(async_await, await_macro, futures_api, tokio_compat)]
+#![feature(async_await, await_macro, futures_api, compat)]
 
 extern crate futures;
-extern crate hyper;
 
-use futures::executor::block_on;
-use futures::future::{self, FutureExt};
-use futures::compat::Compat;
-
-async fn foo(a: &str) {
-    await!(bar(a));
-}
-
-async fn bar(a: &str) {
-    println!("a: {}", a);
-}
+use futures::compat;
 
 fn main() {
-/*   let fut = async {
-    let client = hyper::Client::new();
-    let uri = "https://www.google.com".parse::<hyper::Uri>().unwrap();
-    let resp = await!(client.get(uri).compat());
-    println!("resp: {}", resp);
-  };
-  block_on(fut) */
 }
